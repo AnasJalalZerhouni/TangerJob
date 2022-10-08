f68ca10f586ca65e5d8d850742c995cd289b6793
jayzee <jackobleed@gmail.com>
2022-10-08 17:00:19 +0100

    Initial commit+

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TangerJobV0._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
